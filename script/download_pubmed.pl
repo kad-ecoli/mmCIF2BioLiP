7b702b923b55eef5044874f41168f3b7052e8714
kad-ecoli <cz378@MWPF2YVW8W>
2022-10-21 21:31:47 -0400

    title and abstract only

--- script/download_pubmed.pl
+++ script/download_pubmed.pl
@@ -15,20 +15,46 @@ if (scalar @ARGV==0)
 for (my $a=0;$a<scalar @ARGV;$a++)
 {
     my $pmid=$ARGV[$a];
-    next if (-s "$pmid.txt");
-    system("wget -q 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=$pmid&retmode=abstract&rettype=text' -O $pmid.txt");
-    next if (-s "$pmid.txt");
+    #next if (-s "$pmid.txt");
     system("wget -q 'https://pubmed.ncbi.nlm.nih.gov/$pmid/?format=pubmed' -O $pmid.html");
-    my $txt="";
-    my $pre=0;
+    my $abstract_txt="";
+    my $title_txt="";
+    my $ab=0;
+    my $ti=0;
     foreach my $line(`cat $pmid.html`)
     {
-        if ($line=~/^\s*<pre/) { $pre=1; }
-        elsif ($line=~/^\s*<\/pre>/) { $pre=2; }
-        elsif ($pre==1) { $txt.="$line"; }
+        $line=~s/\r//g;
+        chomp($line);
+        if ($line=~/^AB  - /)
+        {
+            $ab=1;
+            $line=substr($line, 6);
+            $abstract_txt=$line;
+        }
+        elsif ($line=~/^TI  - /)
+        {
+            $ti=1;
+            $line=substr($line, 6);
+            $title_txt=$line;
+        }
+        elsif ($line=~/^\w+\s+- /)
+        {
+            $ab=2;
+            $ti=2;
+        }
+        elsif ($ab==1)
+        {
+            $line=substr($line, 6);
+            $abstract_txt.="$line";
+        }
+        elsif ($ti==1)
+        {
+            $line=substr($line, 6);
+            $title_txt.="$line";
+        }
     }
     open(FP,">$pmid.txt");
-    print FP "$txt";
+    print FP "$title_txt\n$abstract_txt\n";
     close(FP);
     system("rm $pmid.html");
 }
