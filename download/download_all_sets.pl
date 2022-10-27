5f710bb3a76f4e9b3ea661e1d3cfb5f10bf7d34c
kad-ecoli <cz378@MWPF2YVW8W>
2022-10-27 19:45:29 -0400

    download_all_sets.pl

---
+++ download/download_all_sets.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+`mkdir -p BioLiP_updated_set`;
+#`rm -fr BioLiP_updated_set/*`;
+chdir "BioLiP_updated_set";
+
+my $head= "https://zhanggroup.org/BioLiP2/weekly";
+my $address="$head.html";
+system("wget -o log -c $address") == 0 or die "System call failed: $!";
+
+
+my @rst=`cat weekly.html`;
+my @all=();
+foreach my $r(@rst)
+{
+    if($r =~ /\<tr\>\<td\>(\S+)\<\/td\>/)
+    {
+	#print "$1\n";
+	push(@all, $1);
+    }
+}
+my $tot=@all;
+print "\n====================================================\n";
+print "In total, there are $tot weeks to update.\n\n";
+
+my $annotation="BioLiP_UP.txt";
+open(OUT, ">$annotation");
+close(OUT);
+my $annotation1="BioLiP_UP_nr.txt";
+open(OUT, ">$annotation1");
+close(OUT);
+
+foreach my $r(@all)
+{        
+    my $rec="receptor_$r.tar.bz2";
+    my $rec1="receptor1_$r.tar.bz2";
+    my $lig="ligand_$r.tar.bz2";
+    my $ano="BioLiP_$r.txt";
+    my $ano1="BioLiP_$r\_nr.txt";
+
+	print "Dowload redundant set for the week $r...\n";    
+	system("wget -o log -c $head/$rec") == 0 or die "System call failed: $!";
+	system("tar -xvf $rec >log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$rec1") == 0 or die "System call failed: $!";
+	system("tar -xvf $rec1>log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$lig") == 0 or die "System call failed: $!";
+	system("tar -xvf $lig >log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$ano") == 0 or die "System call failed: $!"; 
+	
+	
+	print "Dowload non-redundant set for the week $r...\n";
+	$rec="receptor_$r\_nr.tar.bz2";
+	$rec1="receptor1_$r\_nr.tar.bz2";
+	$lig="ligand_$r\_nr.tar.bz2";
+	system("wget -o log -c $head/$rec") == 0 or die "System call failed: $!";
+	system("tar -xvf $rec >log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$rec1") == 0 or die "System call failed: $!";
+	system("tar -xvf $rec1>log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$lig") == 0 or die "System call failed: $!";
+	system("tar -xvf $lig >log")== 0 or die "System call failed: $!";
+	system("wget -o log -c $head/$ano1") == 0 or die "System call failed: $!"; 
+
+    `cat $ano >> $annotation`;
+    `cat $ano1 >> $annotation1`;
+
+    #last;
+}
+
+
+print "Cheers! All updates are done.\n";
+print "====================================================\n\n";
+
+print "The old set can be download manually at
+https://zhanggroup.org/BioLiP/download.html
+";
